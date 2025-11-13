4d28e332beab7f11a5af6acb9c21548db4ee93a0
Evie-Strings <eugenie.i.viola@gmail.com>
2025-11-13 15:41:34 -0500

    adding setpoint and velocity, starinting simulation

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,25 +4,38 @@
 
 package frc.robot;
 
+import static edu.wpi.first.units.Units.RPM;
+
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.epilogue.NotLogged;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.subsystems.Indexer;
 
 @Logged
 public class RobotContainer {
 
   @NotLogged private final XboxController controller;
 
+  private final Indexer indexer;
+
   public RobotContainer() {
 
     controller = new XboxController(0);
 
+    indexer = new Indexer();
+
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    Trigger test = new Trigger(controller::getAButton);
+
+    test.onTrue(indexer.setSpeedCommand(RPM.of(1000)));
+    test.onFalse(indexer.setSpeedCommand(RPM.of(0)));
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
--- src/main/java/frc/robot/subsystems/Indexer.java
+++ src/main/java/frc/robot/subsystems/Indexer.java
@@ -3,6 +3,7 @@ package frc.robot.subsystems;
 import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.Millimeters;
 import static edu.wpi.first.units.Units.RPM;
+import static edu.wpi.first.units.Units.Volts;
 
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkClosedLoopController;
@@ -16,6 +17,8 @@ import edu.wpi.first.epilogue.NotLogged;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -34,6 +37,13 @@ public class Indexer extends SubsystemBase{
 
   Current current;
 
+  Voltage voltage;
+
+  AngularVelocity velocity;
+
+  Distance laserCANMeasurement;
+
+  AngularVelocity setpoint;
   
   public Indexer() {
     motor = new SparkMax(IndexerConstants.MOTOR_PORT, MotorType.kBrushless);
@@ -46,19 +56,39 @@ public class Indexer extends SubsystemBase{
     config.inverted(false);
 
     closedLoopController = motor.getClosedLoopController();  
+
+    setpoint = RPM.of(0);
     }
 
-   private boolean seePiece() {
-    return laserCAN.getMeasurement().distance_mm<IndexerConstants.MINIMUM_DISTANCE.in(Millimeters);
+   public boolean seePiece() {
+
+    if (laserCAN.getMeasurement()!=null) {
+      return laserCAN.getMeasurement().distance_mm<IndexerConstants.MINIMUM_DISTANCE.in(Millimeters);
+    }
+    
+    else {
+      return false;
+    }
    }
 
   private void setSpeed(AngularVelocity speed) {
     closedLoopController.setReference(speed.in(RPM), ControlType.kVelocity);
+    setpoint = speed;
   }
 
   @Override
   public void periodic() {
     current = Amps.of(motor.get());
+    voltage = Volts.of(motor.getAppliedOutput()*motor.getBusVoltage());
+    velocity = RPM.of(motor.getEncoder().getVelocity());
+
+    if (laserCAN.getMeasurement()!=null) {
+      laserCANMeasurement = Millimeters.of(laserCAN.getMeasurement().distance_mm);
+    }
+  }
+
+  public void simulationPeriodic() {
+    velocity = setpoint;
   }
 
   public Command setSpeedCommand(AngularVelocity speed) {
