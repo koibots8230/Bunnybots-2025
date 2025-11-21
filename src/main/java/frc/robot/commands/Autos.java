67a6a261098c15a8ee45147f4e7a492c97263a9c
JakeStew81 <jakestewart711@icloud.com>
2025-11-20 19:00:55 -0500

    Basic autos

---
+++ src/main/java/frc/robot/commands/Autos.java
@@ -0,0 +1,50 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.subsystems.Indexer;
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Swerve;
+
+public class Autos {
+
+  public static Command leave(Swerve swerve) {
+    return swerve.autoDriveCommand(
+        AutoConstants.LEAVE_DISTANCE, AutoConstants.LEAVE_VELOCITY, AutoConstants.LEAVE_HEADING);
+  }
+
+  public static Command scoreHigh(Shooter shooter, Indexer indexer) {
+    return Commands.sequence(
+        Commands.race(
+            ScoringCommands.shootHigh(indexer, shooter),
+            Commands.waitTime(AutoConstants.SHOOTING_TIME)),
+        ScoringCommands.stop(shooter));
+  }
+
+  public static Command scoreHighLeave(Shooter shooter, Indexer indexer, Swerve swerve) {
+    return Commands.sequence(
+        Commands.race(
+            ScoringCommands.shootHigh(indexer, shooter),
+            Commands.waitTime(AutoConstants.SHOOTING_TIME)),
+        ScoringCommands.stop(shooter),
+        Autos.leave(swerve));
+  }
+
+  public static Command scoreLow(Shooter shooter, Indexer indexer) {
+    return Commands.sequence(
+        Commands.race(
+            ScoringCommands.shootLow(indexer, shooter),
+            Commands.waitTime(AutoConstants.SHOOTING_TIME)),
+        ScoringCommands.stop(shooter));
+  }
+
+  public static Command scoreLowLeave(Shooter shooter, Indexer indexer, Swerve swerve) {
+    return Commands.sequence(
+        Commands.race(
+            ScoringCommands.shootLow(indexer, shooter),
+            Commands.waitTime(AutoConstants.SHOOTING_TIME)),
+        ScoringCommands.stop(shooter),
+        Autos.leave(swerve));
+  }
+}
--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -134,6 +134,11 @@ public class Swerve extends SubsystemBase {
 
   @Override
   public void simulationPeriodic() {
+    modules.frontLeft.updateLogs();
+    modules.frontRight.updateLogs();
+    modules.backLeft.updateLogs();
+    modules.backRight.updateLogs();
+
     simHeading =
         simHeading.plus(
             new Rotation2d(
@@ -250,14 +255,12 @@ public class Swerve extends SubsystemBase {
             () -> {
               autoStartingPosition = estimatedPosition.getTranslation();
             }),
-        Commands.print("AutoStartpos done"),
         Commands.race(
             Commands.run(() -> this.followVector(velocity, heading), this),
             Commands.waitUntil(
                 () ->
                     estimatedPosition.getTranslation().getDistance(autoStartingPosition)
                         >= distance.in(Meters))),
-        Commands.print("Loop done"),
         Commands.runOnce(() -> this.followVector(MetersPerSecond.of(0), Rotation2d.kZero), this));
   }
 
